6bf5d94c1dd6eef35d4e7d310f8b64d00660f0d8
Santosh Kumar Singh <development@amplework.com>
2021-07-12 14:09:38 +0530

    first commit

---
+++ android/app/src/main/java/com/rnboilerplate/MainActivity.java
@@ -0,0 +1,26 @@
+package com.rnboilerplate;
+
+import com.facebook.react.ReactActivity;
+import android.content.Intent;
+import android.content.res.Configuration;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "RNBoilerPlate";
+  }
+
+  // for react-native-appearance
+  @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+      super.onConfigurationChanged(newConfig);
+      Intent intent = new Intent("onConfigurationChanged");
+      intent.putExtra("newConfig", newConfig);
+      sendBroadcast(intent);
+    }
+}
