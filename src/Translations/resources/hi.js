56726168fd83395e399e8846fd09574bd20bef50
Arunabh Verma <arunabhverm01@gmail.com>
2021-07-12 15:36:12 +0530

    adding new lang hi

---
+++ src/Translations/resources/hi.js
@@ -0,0 +1,12 @@
+export default {
+  welcome: 'TheCodingMachine द्वारा रिएक्ट नेटिव RNBoilerPlate में आपका स्वागत है',
+  actions: {
+    continue: 'जारी रखें',
+  },
+  example: {
+    helloUser: 'मैं एक नकली उपयोगकर्ता हूँ, मेरा नाम {{name}} है',
+    labels: {
+      userId: 'एक उपयोगकर्ता आईडी दर्ज करें',
+    },
+  },
+}
