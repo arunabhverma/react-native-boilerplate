9f884b834d42589469766788ac525ecac8096811
Prakash Chand Saini <prakash@amplework.com>
2021-07-15 18:06:06 +0530

    game created tiktaktoe

--- src/Navigators/Main.js
+++ src/Navigators/Main.js
@@ -1,14 +1,18 @@
 import React from 'react'
+import { LogBox } from 'react-native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { IndexExampleContainer } from '@/Containers'
+import { TikTakToe } from '@/Containers'
 
 const Tab = createBottomTabNavigator()
 
+LogBox.ignoreAllLogs();
 // @refresh reset
 const MainNavigator = () => {
   return (
     <Tab.Navigator>
       <Tab.Screen name="Home" component={IndexExampleContainer} />
+      <Tab.Screen name="TikTakToe" component={TikTakToe} />
     </Tab.Navigator>
   )
 }
